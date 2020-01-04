7b8ca57657a142baca865ae5dbcb6a5e0451140d
Mason <mason@thewithers.org>
2020-01-04 21:07:03 +0000

    Replace Rocket.py

--- Rocket.py
+++ Rocket.py
@@ -1,32 +1,43 @@
-#!/usr/bin/python
-import time
-launch = ""
 
-print("Super Nova mission setup")
+
+import time
+import random
+launch = ""
+x = random.randint(1,100)
+
+
+
+Rocketname = raw_input("what is the Rocket called: ")
+
+print( Rocketname + " mission setup")
 destination = raw_input("what is the destination: ")
 launchTime = raw_input("what is the launch time: ")
 missionName = raw_input("what is the mission name: ")
 payload = raw_input("what is the payload: ")
 launchSite = raw_input("what is the launch site: ")
+payloaddestination = raw_input("what is the payload destination: ")
 
 while not launch == 'y':
   launch = raw_input("ready for launch? (y/n) ")
+  time.sleep(2.5)
 
 
-
-print("Starting Super Nova " + missionName +" launch sequence ")
-print("destination is " + destination )
-print("launchTime is " + launchTime)
-print("missionName is " + missionName)
-print("payload is " + payload)
-print("launchSite is " + launchSite    )
-print("3")
-time.sleep(1)
-print("2")
-time.sleep(1)
-print("1")
-time.sleep(1)
-print("liftoff")
-time.sleep(10)
-print("reached "+ desination )
-print("Would you like to do it again ?(y/n)")
+print(" Starting " + Rocketname +  missionName + " launch sequence ")
+if x <= 10:
+  print("Rocket exploded")
+else:
+  print("destination is " + destination )
+  print("launchTime is " + launchTime)
+  print("missionName is " + missionName)
+  print("payload is " + payload)
+  print("launchSite is " + launchSite    )
+  print("3")
+  time.sleep(1)
+  print("2")
+  time.sleep(1)
+  print("1")
+  time.sleep(1)
+  print("liftoff")
+  time.sleep(5)
+  print("reached "+ destination ) 
+  print( payload +" got to "  + payloaddestination   )
